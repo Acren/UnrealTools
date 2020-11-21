51cb86ad7cf88f4495efc34c42a9c84fbcc4f88a
Sam Bonifacio <sam@bonifacio.name>
2020-11-21 18:55:06 +1100

    Handle git.exe not found, and hardcode path for now

--- UnrealAutomationCommon/VersionControlUtils.cs
+++ UnrealAutomationCommon/VersionControlUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
 namespace UnrealAutomationCommon
 {
@@ -6,7 +7,8 @@ namespace UnrealAutomationCommon
     {
         public static string GetBranchName(string WorkingDirectory)
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo("git.exe");
+            string gitPath = @"C:\Program Files\Git\bin\git.exe";
+            ProcessStartInfo startInfo = new ProcessStartInfo(gitPath);
 
             startInfo.UseShellExecute = false;
             startInfo.WorkingDirectory = WorkingDirectory ?? "dir Here";
@@ -16,7 +18,22 @@ namespace UnrealAutomationCommon
 
             Process process = new Process();
             process.StartInfo = startInfo;
-            process.Start();
+
+            try
+            {
+                process.Start();
+            }
+            catch (Win32Exception Ex)
+            {
+                if (Ex.NativeErrorCode == 2)
+                {
+                    throw new System.Exception("git.exe was not found or access was denied, is it installed?", Ex);
+                }
+                else
+                {
+                    throw Ex;
+                }
+            }
 
             string branchName = process.StandardOutput.ReadLine();
             return branchName;
