70cc071deab3df8692810e88c774c9cbeef85811
Sam Bonifacio <sam@bonifacio.name>
2021-07-12 17:59:01 +1000

    Add logs with process and operation names

--- UnrealAutomationCommon/Operations/OperationRunner.cs
+++ UnrealAutomationCommon/Operations/OperationRunner.cs
@@ -16,7 +16,9 @@ namespace UnrealAutomationCommon.Operations
         private Operation _operation;
         private OperationParameters _operationParameters;
         private Process _process;
+        private string _processName;
         private bool _isWaitingForLogs = false;
+        private bool _terminated = false;
 
         public event OperationOutputEventHandler Output;
         public event OperationEndedEventHandler Ended;
@@ -52,6 +54,9 @@ namespace UnrealAutomationCommon.Operations
                 OnProcessEnded();
             });
 
+            _processName = _process.ProcessName;
+            Output?.Invoke("Launched process '" + _processName + "'", LogVerbosity.Log);
+
             FlagOptions flagOptions = _operationParameters.FindOptions<FlagOptions>();
             if(flagOptions.WaitForAttach)
             {
@@ -61,7 +66,8 @@ namespace UnrealAutomationCommon.Operations
 
         public void Terminate()
         {
-            Output?.Invoke("Operation terminated", LogVerbosity.Log);
+            _terminated = true;
+            Output?.Invoke("Terminating operation '" + _operation.OperationName + "'", LogVerbosity.Warning);
             ProcessUtils.KillProcessAndChildren(_process);
         }
 
@@ -119,7 +125,7 @@ namespace UnrealAutomationCommon.Operations
             OperationResult result = new OperationResult();
             result.ExitCode = _process.ExitCode;
 
-            Output?.Invoke("Process exited with code " + result.ExitCode, result.ExitCode == 0 ? LogVerbosity.Log : LogVerbosity.Error);
+            Output?.Invoke("Process '"+ _processName + "' exited with code " + result.ExitCode, result.ExitCode == 0 ? LogVerbosity.Log : LogVerbosity.Error);
 
             AutomationOptions automationOptions = _operationParameters.FindOptions<AutomationOptions>();
             if (automationOptions.RunTests)
@@ -140,9 +146,9 @@ namespace UnrealAutomationCommon.Operations
                     foreach (Test test in result.TestReport.Tests)
                     {
                         Output?.Invoke(EnumUtils.GetName(test.State).ToUpperInvariant().PadRight(7) + " - " + test.FullTestPath, test.State == TestState.Success ? LogVerbosity.Log : LogVerbosity.Error);
-                        foreach(TestEntry entry in test.Entries)
+                        foreach (TestEntry entry in test.Entries)
                         {
-                            if(entry.Event.Type != TestEventType.Info)
+                            if (entry.Event.Type != TestEventType.Info)
                             {
                                 Output?.Invoke("".PadRight(9) + " - " + entry.Event.Message, entry.Event.Type == TestEventType.Error ? LogVerbosity.Error : LogVerbosity.Warning);
                             }
@@ -153,6 +159,8 @@ namespace UnrealAutomationCommon.Operations
                     Output?.Invoke(testsPassed + " of " + result.TestReport.Tests.Count + " tests passed", allPassed ? LogVerbosity.Log : LogVerbosity.Error);
                 }
             }
+
+            Output?.Invoke("Operation '" +  _operation.OperationName + "' " + (_terminated ? "terminated" : "completed"), _terminated ? LogVerbosity.Warning : LogVerbosity.Log);
 
             Ended?.Invoke(result);
         }
