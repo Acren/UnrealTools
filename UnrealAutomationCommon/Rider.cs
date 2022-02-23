852d27318ff7ea8eff6ce47236a83b86adde5c3c
Sam Bonifacio <sam@bonifacio.name>
2022-02-23 21:50:41 +1100

    Use "JetBrains Rider" registry key to find Rider path if "Rider for Unreal Engine" not found

--- UnrealAutomationCommon/Rider.cs
+++ UnrealAutomationCommon/Rider.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Win32;
+﻿using System;
+using Microsoft.Win32;
 using System.IO;
+using System.Linq;
 
 namespace UnrealAutomationCommon
 {
@@ -8,18 +10,50 @@ namespace UnrealAutomationCommon
         public static string FindPath()
         {
             RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey jetBrains = localMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\JetBrains");
 
-            RegistryKey localMachineRider = localMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\JetBrains\Rider for Unreal Engine");
+            string[] subKeyNames = jetBrains.GetSubKeyNames();
 
-            string subkeyName = localMachineRider.GetSubKeyNames()[0];
-            RegistryKey version = localMachineRider.OpenSubKey(subkeyName);
-            var path = version.GetValue(null) as string;
+            RegistryKey riderVersionKey = GetRiderVersionKey(jetBrains, "Rider for Unreal Engine");
+
+            if (riderVersionKey is null)
+            {
+                riderVersionKey = GetRiderVersionKey(jetBrains, "JetBrains Rider");
+            }
+
+            if (riderVersionKey is null)
+            {
+                throw new Exception("Couldn't find Rider path in registry");
+            }
+
+            var path = riderVersionKey.GetValue(null) as string;
             return path;
         }
 
         public static string FindExePath()
         {
             return Path.Combine(FindPath(), "bin", "rider64.exe");
+        }
+
+        private static RegistryKey GetRiderVersionKey(RegistryKey jetBrains, string riderKeyName)
+        {
+            string[] subKeyNames = jetBrains.GetSubKeyNames();
+
+            if (!subKeyNames.Contains(riderKeyName))
+            {
+                return null;
+            }
+
+            RegistryKey localMachineRider = jetBrains.OpenSubKey(riderKeyName);
+
+            string[] riderSubKeyNames = localMachineRider.GetSubKeyNames();
+
+            if (riderSubKeyNames.Length == 0)
+            {
+                return null;
+            }
+
+            return localMachineRider.OpenSubKey(riderSubKeyNames[0]);
         }
     }
 }
