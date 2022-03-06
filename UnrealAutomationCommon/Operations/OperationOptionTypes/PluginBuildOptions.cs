7beb018c0acea057461ec9b4cf5a6b92c43a337e
Sam Bonifacio <sam@bonifacio.name>
2022-03-06 17:16:08 +1100

    Plugin build options before automation

--- UnrealAutomationCommon/Operations/OperationOptionTypes/PluginBuildOptions.cs
+++ UnrealAutomationCommon/Operations/OperationOptionTypes/PluginBuildOptions.cs
@@ -2,7 +2,7 @@
 {
     public class PluginBuildOptions : OperationOptions
     {
-        public override int Index => 70;
+        public override int Index => 45;
 
         public Option<bool> BuildWin64 { get; } = true;
         public Option<bool> BuildLinux { get; } = true;
