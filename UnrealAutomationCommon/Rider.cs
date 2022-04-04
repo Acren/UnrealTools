e7f500b398e9177b2674d59f945aa70d89d822c3
Sam Bonifacio <sam@bonifacio.name>
2022-04-04 20:59:35 +1000

    Find Rider installations in CurrentUser

--- UnrealAutomationCommon/Rider.cs
+++ UnrealAutomationCommon/Rider.cs
@@ -9,16 +9,36 @@ namespace UnrealAutomationCommon
     {
         public static string FindPath()
         {
-            RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-            RegistryKey jetBrains = localMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\JetBrains");
-
-            string[] subKeyNames = jetBrains.GetSubKeyNames();
-
-            RegistryKey riderVersionKey = GetRiderVersionKey(jetBrains, "Rider for Unreal Engine");
+            RegistryKey riderVersionKey = GetRiderVersionKey("Rider for Unreal Engine");
 
             if (riderVersionKey is null)
             {
-                riderVersionKey = GetRiderVersionKey(jetBrains, "JetBrains Rider");
+                riderVersionKey = GetRiderVersionKey("JetBrains Rider");
+            }
+
+            if (riderVersionKey is null)
+            {
+                RegistryKey currentUser = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
+                RegistryKey rider = currentUser.OpenSubKey(@"SOFTWARE\JetBrains\Rider");
+                string[] subKeyNames = rider.GetSubKeyNames();
+                string bestInstallDir = null;
+                int bestProductVersion = 0;
+                foreach (string subKeyName in subKeyNames)
+                {
+                    RegistryKey subKey = rider.OpenSubKey(subKeyName);
+                    string installDir = subKey.GetValue("InstallDir") as string;
+                    if(installDir == null)
+                    {
+                        continue;
+                    }
+                    int productVersion = int.Parse(subKey.GetValue("ProductVersion") as string);
+                    if(installDir != null && productVersion > bestProductVersion)
+                    {
+                        bestInstallDir = installDir;
+                        bestProductVersion = productVersion;
+                    }
+                }
+                return bestInstallDir;
             }
 
             if (riderVersionKey is null)
@@ -35,8 +55,11 @@ namespace UnrealAutomationCommon
             return Path.Combine(FindPath(), "bin", "rider64.exe");
         }
 
-        private static RegistryKey GetRiderVersionKey(RegistryKey jetBrains, string riderKeyName)
+        private static RegistryKey GetRiderVersionKey(string riderKeyName)
         {
+            RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey jetBrains = localMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\JetBrains");
+
             string[] subKeyNames = jetBrains.GetSubKeyNames();
 
             if (!subKeyNames.Contains(riderKeyName))
