0940d22b3f8fbf390734b5933d9dc4e89c3afa61
Sam Bonifacio <sam@bonifacio.name>
2023-08-19 14:40:24 +1000

    Launch editor without project

--- UnrealAutomationCommon/Operations/OperationTypes/LaunchEditor.cs
+++ UnrealAutomationCommon/Operations/OperationTypes/LaunchEditor.cs
@@ -3,11 +3,15 @@ using UnrealAutomationCommon.Unreal;
 
 namespace UnrealAutomationCommon.Operations.OperationTypes
 {
-    public class LaunchEditor : UnrealProcessOperation<Project>
+    public abstract class LaunchEditor<T> : UnrealProcessOperation<T> where T : OperationTarget, IEngineInstanceProvider
     {
         protected override Command BuildCommand(OperationParameters operationParameters)
         {
             return new Command(GetTargetEngineInstall(operationParameters).GetEditorExe(operationParameters), UnrealArguments.MakeArguments(operationParameters, GetOutputPath(operationParameters), true));
         }
     }
+
+    public class LaunchProjectEditor : LaunchEditor<Project> { }
+
+    public class LaunchEditor : LaunchEditor<Engine> { }
 }
