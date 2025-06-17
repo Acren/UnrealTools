6c3a90b306d98d587401c8d7226c57a9146c873f
Sam Bonifacio <sam@bonifacio.name>
2025-06-18 00:31:39 +1000

    Remove unnecessary check that was only catching an issue caused by erroneous plugin handling in the DeployPlugin operation

--- UnrealAutomationCommon/Operations/OperationTypes/PackageProject.cs
+++ UnrealAutomationCommon/Operations/OperationTypes/PackageProject.cs
@@ -43,20 +43,6 @@ namespace UnrealAutomationCommon.Operations.OperationTypes
             return new Command(GetTargetEngineInstall(operationParameters).GetRunUATPath(), arguments);
         }
 
-        protected override Task<OperationResult> OnExecuted(CancellationToken token)
-        {
-            Project targetProject = GetTarget(OperationParameters);
-            // Only check for plugins with runtime modules, as editor-only plugins won't be included in packaged builds
-            List<Plugin> codePluginsWithRuntimeModules = targetProject.Plugins.Where(p => p.HasRuntimeModules).ToList();
-            if (targetProject.IsBlueprintOnly && codePluginsWithRuntimeModules.Count > 0)
-            {
-                // Note: The specific error this causes is "Plugin X failed to load because module Y could not be found" when launching the package
-                throw new Exception($"Blueprint-only project contains {codePluginsWithRuntimeModules.Count} code plugin(s) with runtime modules directly ({string.Join(", ",codePluginsWithRuntimeModules.Select(p => p.Name))}). Although the engine will normally allow this, it will cause errors that are difficult to debug. Install the plugin to the engine instead.");
-            }
-
-            return base.OnExecuted(token);
-        }
-
         protected override string GetOperationName()
         {
             return "Package Project";
